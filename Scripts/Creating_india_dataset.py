0f2b950ee9df5a309f03a87200178dbc95684e83
AshwiniJatti <ashwini.jatti@gmail.com>
2025-01-24 14:04:05 +0100

    Scripts added

--- Scripts/Creating_india_dataset.py
+++ Scripts/Creating_india_dataset.py
@@ -6,7 +6,12 @@ data = pd.read_csv("C:/Ashwini/Hobby Project/2024YTChannels/Data/youtube_channel
 #Creating a separate dataset for country India
 data_india = data[data['country'] == 'India']
 
+#Dropping null values from the dataset
+filtered_data_india = data_india.dropna()
+
+#filtered_data_india = filtered_data_india.loc[filtered_data_india['description'].str.contains('food')]
+
 #Writing a dataframe to the csv file
-data_india.to_csv('C:/Ashwini/Hobby Project/2024YTChannels/Data/youtube_channels_india.csv')
+filtered_data_india.to_csv('C:/Ashwini/Hobby Project/2024YTChannels/Data/youtube_channels_india.csv')
 
 
